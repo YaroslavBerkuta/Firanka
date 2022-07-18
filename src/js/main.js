f118ad5250b5aff0683bc0345df5badb4b5e1956
YaroslavBerkuta <yaroslavberkuta@gmail.com>
2022-07-18 17:13:31 +0300

    cart page

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,7 @@
-import Swiper, { Pagination, EffectFade } from "swiper";
+import Swiper, { Pagination, EffectFade, Thumbs } from "swiper";
 import { Fancybox } from "@fancyapps/ui";
 import "swiper/css/bundle";
-
+import $ from "jquery";
 new Swiper(".hero__right", {
   modules: [Pagination, EffectFade],
   pagination: {
@@ -20,3 +20,39 @@ new Swiper(".catalog__slider", {
 });
 
 Fancybox.bind("[data-fancybox]", {});
+
+const filterItem = document.querySelectorAll(".wrapper__item");
+filterItem.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+  });
+});
+const galleryThumb = new Swiper(".gallery__thumbs", {
+  spaceBetween: 10,
+  direction: "vertical",
+  freeMode: true,
+  slidesPerView: 6,
+});
+const galleryMain = new Swiper(".gallery__main", {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  modules: [Thumbs],
+  thumbs: {
+    swiper: galleryThumb,
+  },
+});
+
+$(".tovar__cart label span:first-child").click(function () {
+  var $input = $(this).parent().find("input");
+  var count = parseInt($input.val()) - 1;
+  count = count < 1 ? 1 : count;
+  $input.val(count);
+  $input.change();
+  return false;
+});
+$(".tovar__cart label span:last-child").click(function () {
+  var $input = $(this).parent().find("input");
+  $input.val(parseInt($input.val()) + 1);
+  $input.change();
+  return false;
+});
